3f17b663f970dae594906559bab681678e4fca03
theooprea <opreatheodor54@gmail.com>
2022-11-12 22:02:49 +0200

    Add main to server
    Signed-off-by: theooprea <opreatheodor54@gmail.com>

--- tema1/oauth_server.c
+++ tema1/oauth_server.c
@@ -3,6 +3,7 @@
 #include <rpc/rpc.h> 
 
 #include "oauth.h"
+#include "oauth_svc.h"
 #include "helpers.h"
 
 request_auth_response *request_auth_1_svc(char **user_id, struct svc_req *cl) {
@@ -13,4 +14,36 @@ request_auth_response *request_auth_1_svc(char **user_id, struct svc_req *cl) {
     strcpy(response.token, *user_id);
 
     return &response;
+}
+
+int main (int argc, char **argv)
+{
+	register SVCXPRT *transp;
+
+	pmap_unset (SERVER, SERVERVERSION);
+
+	transp = svcudp_create(RPC_ANYSOCK);
+	if (transp == NULL) {
+		fprintf (stderr, "%s", "cannot create udp service.");
+		exit(1);
+	}
+	if (!svc_register(transp, SERVER, SERVERVERSION, server_1, IPPROTO_UDP)) {
+		fprintf (stderr, "%s", "unable to register (SERVER, SERVERVERSION, udp).");
+		exit(1);
+	}
+
+	transp = svctcp_create(RPC_ANYSOCK, 0, 0);
+	if (transp == NULL) {
+		fprintf (stderr, "%s", "cannot create tcp service.");
+		exit(1);
+	}
+	if (!svc_register(transp, SERVER, SERVERVERSION, server_1, IPPROTO_TCP)) {
+		fprintf (stderr, "%s", "unable to register (SERVER, SERVERVERSION, tcp).");
+		exit(1);
+	}
+
+	svc_run ();
+	fprintf (stderr, "%s", "svc_run returned");
+	exit (1);
+	/* NOTREACHED */
 }
